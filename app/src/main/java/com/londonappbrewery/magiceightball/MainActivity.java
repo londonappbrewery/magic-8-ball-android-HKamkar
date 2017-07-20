b721c80a911f12a820c5c1541303fc331604efb4
Hossein Kamkar <soosha.com@gmail.com>
2017-07-20 17:20:54 +0430

    Project Done
    Add Change theme option to the app!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,69 @@
 package com.londonappbrewery.magiceightball;
 
+import android.provider.SyncStateContract;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static com.londonappbrewery.magiceightball.R.drawable.background_1;
+import static com.londonappbrewery.magiceightball.R.drawable.background_2;
 
 public class MainActivity extends AppCompatActivity {
+  //Fields
+  ConstraintLayout layout;
+  Button btn_ask;
+  Button btn_theme;
+  ImageView img_8ball;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    layout = (ConstraintLayout) findViewById(R.id.main);
+    btn_ask = (Button) findViewById(R.id.btn_ask);
+    btn_theme = (Button) findViewById(R.id.btn_background);
+    img_8ball = (ImageView) findViewById(R.id.img_8bal);
+    final int[] ballArray = {R.drawable.ball1,
+      R.drawable.ball2,
+      R.drawable.ball3,
+      R.drawable.ball4,
+      R.drawable.ball5};
+
+    // Click on Ask Button
+    btn_ask.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+       int randomNumber = new Random().nextInt(5);
+        Log.i("8ball", "the number is: " + randomNumber);
+        img_8ball.setImageResource(ballArray[randomNumber]);
+      }
+    });
+
+    // Change theme
+    btn_theme.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        changeTheme();
+      }
+    });
+  }
+
+  public void changeTheme() {
+    if (layout.getBackground().getConstantState().equals(getResources().getDrawable(background_1).getConstantState())) {
+      layout.setBackgroundResource(background_2);
+      btn_theme.setBackgroundColor(getResources().getColor(R.color.button_color2));
+      btn_ask.setBackgroundColor(getResources().getColor(R.color.button_color2));
+    } else {
+      layout.setBackgroundResource(background_1);
+      btn_theme.setBackgroundColor(getResources().getColor(R.color.button_color));
+      btn_ask.setBackgroundColor(getResources().getColor(R.color.button_color));
     }
+  }
 }
